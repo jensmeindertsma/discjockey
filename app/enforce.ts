1e4a0296bfca1cb7b7f77eccce41d6253a27604a
Jens Meindertsma <mail@jensmeindertsma.com>
2024-03-10 15:01:00 +0100

    oops

--- app/enforce.ts
+++ app/enforce.ts
@@ -1,5 +1,7 @@
 export function enforce<T>(value: T | null | undefined, message: string): T {
   if (!value) {
     throw new Error(`Failed to enforce requirement: "${message}"`);
+  } else {
+    return value;
   }
 }
